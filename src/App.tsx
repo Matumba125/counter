d1a147e7e3d43a243e6a73eb7cf97554a2677e15
matumba 125 <nbenza5@gmail.com>
2021-06-20 15:44:00 +0300

    created counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Counter} from "./components/Counter/Counter";
+import {Button} from "./components/Button/Button";
+
+//export type ValueType = 0 | 1 | 2 | 3 | 4 | 5;
+
+export type ValueType = number;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const maxValue = 5;
+    const InitValue = 0;
+
+    let [value, setValue] = useState<ValueType>(InitValue);
+
+    function IncCounter() {
+        setValue(value + 1)
+    }
+
+    function ResetCounter() {
+        setValue(0);
+    }
+
+    return (
+        <div className={'App'}>
+            <Counter value={value} maxValue={maxValue}/>
+            <div className={'buttons'}>
+                <Button
+                    callBack={IncCounter}
+                    value={value}
+                    innerText={'inc'}
+                    disableValue={maxValue}
+                />
+                <Button
+                    callBack={ResetCounter}
+                    value={value}
+                    innerText={'reset'}
+                    disableValue={InitValue}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
